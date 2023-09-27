95acd89536e7f2777b3e912670da4f5cac2b6bad
moozzyk <moozzyk@gmail.com>
2023-09-26 22:49:27 -0700

    Simplify example

--- std-optional-bool/1_Initial/initial.cc
+++ std-optional-bool/1_Initial/initial.cc
@@ -2,10 +2,9 @@
 #include <optional>
 #include <string>
 
-bool isMorning() { return false; }
-
 int main() {
-  if (isMorning()) {
+  bool isMorning = false;
+  if (isMorning) {
     std::cout << "Good Morning!" << std::endl;
   } else {
     std::cout << "Good Afternoon" << std::endl;
